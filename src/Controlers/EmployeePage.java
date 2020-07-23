e56ebb7e27ffe1e9584d29e2f710e121640c91ae
kader <akader36@gmail.com>
2020-07-24 00:06:15 +0100

    تحديث جديد مهدي

--- src/Controlers/EmployeePage.java
+++ src/Controlers/EmployeePage.java
@@ -1,13 +1,76 @@
 package Controlers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class EmployeePage {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeePage implements Initializable {
+
+    @FXML
+    private TextField employeeName;
+
+    @FXML
+    private ComboBox<?> locationName;
+
+    @FXML
+    private TextField identityNumber;
+
+    @FXML
+    private TextField employeeNumber;
+
+    @FXML
+    private TextField employeeNationality;
+
+    @FXML
+    private TextField religion;
+
+    @FXML
+    private TextField residenceOccupation;
+
+    @FXML
+    private ComboBox<?> identityType;
+
+    @FXML
+    private ComboBox<?> reelOccupation;
+
+    @FXML
+    private DatePicker residenceEndDate;
+
+    @FXML
+    private DatePicker HealthCertificateStartDate;
+
+    @FXML
+    private DatePicker HealthCertificatEndDate;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+
+    @FXML
+    void addEmployee(ActionEvent event) {
+
+    }
+
+
+
+
     public void areas(javafx.event.ActionEvent actionEvent) {
         try {
 
@@ -133,4 +196,6 @@ public class EmployeePage {
         }
 
     }
+
+
 }
