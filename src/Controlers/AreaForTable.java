1f621aef0675b8300ee9bf1906dc507f21a22e75
kader <akader36@gmail.com>
2020-07-24 16:14:12 +0100

    commit

---
+++ src/Controlers/AreaForTable.java
@@ -0,0 +1,19 @@
+package Controlers;
+
+public class AreaForTable {
+    private String areaName;
+    private String areaId;
+
+    public AreaForTable(String areaName, String areaId) {
+        this.areaName = areaName;
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public String getAreaId() {
+        return areaId;
+    }
+}
