ec2831465f03c1980bc2039c343393f1470d39da
kader <akader36@gmail.com>
2020-07-24 01:50:18 +0100

    تحديث جديد مهدي

--- src/Controlers/User.java
+++ src/Controlers/User.java
@@ -1,4 +1,49 @@
 package Controlers;
 
 public class User {
+    private int idUser,idArea,idLocation,privilegesId;
+    private String username,password,phoneNumber,employeeNumber;
+
+    public User(int idUser, int idArea, int idLocation, int privilegesId, String username, String password, String phoneNumber, String employeeNumber) {
+        this.idUser = idUser;
+        this.idArea = idArea;
+        this.idLocation = idLocation;
+        this.privilegesId = privilegesId;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public int getIdArea() {
+        return idArea;
+    }
+
+    public int getIdLocation() {
+        return idLocation;
+    }
+
+    public int getPrivilegesId() {
+        return privilegesId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
 }
